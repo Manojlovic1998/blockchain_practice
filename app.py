830be582cfbc7fafcea099cb6845a590d88c53f7
Manojlovic1998 <manojlovic911@hotmail.com>
2021-08-01 20:34:59 +0200

    Create two routes that make use of the blockchain model

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from modules.mod_bchain import models
 app = Flask(__name__)
 
+blockchain = models.Blockchain()
 
-@app.route("/")
-def index_page():
-    return "<p>Hello World!</p>"
+
+@app.route("/mine_block")
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block["proof"]
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {"message": "Congratulations, you just mined a block!",
+                "index": block["index"],
+                "timestamp": block['timestamp'],
+                "proof": block["proof"],
+                "previous_hash": block["previous_hash"]}
+
+    return jsonify(response), 200
+
+
+@app.route("/get_chain")
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response)
+
+
+
+
