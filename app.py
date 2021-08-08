885f1a0b504b4310734c6cd1dd2af0c9cbc1a57d
Manojlovic1998 <manojlovic911@hotmail.com>
2021-08-08 02:20:18 +0200

    Add node_address and add transaction to the mine_block view

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from modules.mod_bchain import models
+from uuid import uuid4
+
 app = Flask(__name__)
 
+# Creating Blockchain
 blockchain = models.Blockchain()
+
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
 
 
 @app.route("/mine_block")
 def mine_block():
+    # Get information from the blockchain stored into variables
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block["proof"]
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    # Add Transaction
+    blockchain.add_transaction(sender=node_address, receiver="Nemanja", amount=16)
+    # Create block
     block = blockchain.create_block(proof, previous_hash)
+
     response = {"message": "Congratulations, you just mined a block!",
                 "index": block["index"],
                 "timestamp": block['timestamp'],
                 "proof": block["proof"],
-                "previous_hash": block["previous_hash"]}
+                "previous_hash": block["previous_hash"],
+                "transactions": block["transactions"]
+                }
 
     return jsonify(response), 200
 
